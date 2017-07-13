f2219e2083a2a6377b5f69cf4189a8ad02a14afb
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-07-14 00:01:55 +0200

    Added support for Jaw textures

--- Source/DontShaveYourHead/Harmony_PawnGraphicSet.cs
+++ Source/DontShaveYourHead/Harmony_PawnGraphicSet.cs
@@ -26,19 +26,27 @@ namespace DontShaveYourHead
             {
                 pathAppendString = "UpperHead";
             }
+            else if (pawn.apparel.BodyPartGroupIsCovered(DefDatabase<BodyPartGroupDef>.GetNamed("Teeth")))
+            {
+                pathAppendString = "Jaw";
+            }
 
             // Set hair graphics to headgear-appropriate texture
             var texPath = pawn.story.hairDef.texPath;
             if (!pathAppendString.NullOrEmpty())
             {
                 // Check if the path exists
-                texPath += "/" + pathAppendString;
-                if (!ContentFinder<Texture2D>.Get(texPath + "_front", false))
+                var newTexPath = texPath + "/" + pathAppendString;
+                if (!ContentFinder<Texture2D>.Get(newTexPath + "_front", false))
                 {
 #if DEBUG
                     Log.Warning("DSYH :: could not find texture at " + texPath);
 #endif
-                    texPath = HairDefOf.Shaved.texPath;
+                    if (pathAppendString != "Jaw") texPath = HairDefOf.Shaved.texPath;
+                }
+                else
+                {
+                    texPath = newTexPath;
                 }
             }
             __instance.hairGraphic = GraphicDatabase.Get<Graphic_Multi>(texPath, ShaderDatabase.Cutout, Vector2.one, pawn.story.hairColor); // Set new graphic
